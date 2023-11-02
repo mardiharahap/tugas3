65e08ef300750ca7c1122f28df4a92db8fba630c
mardiharahap <134383375+mardiharahap@users.noreply.github.com>
2023-11-02 18:19:00 +0700

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+const restaurant = {
+  name: "Lion X Restaurant",
+  city: "Bali",
+  favoriteDrink: "Thai Tea",
+  favoriteFood: "Steak",
+  isVegan: true,
+};
+
+const name = restaurant.name;
+const favoriteDrink = restaurant.favoriteDrink;
+
+console.log("Nama Restoran:", restaurant.name);
+console.log("Minuman Favorit:", restaurant.favoriteDrink);
